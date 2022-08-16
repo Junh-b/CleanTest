b6efa4bf8ae9741a34e2d31c92160405040a9708
Junh <jooha208@gmail.com>
2022-08-16 12:32:26 +0900

    fix do not use RequestDto VO object in Acceptance Test
    instead AcceptanceTest will request with ObjectNode(jackson Json Object)

--- src/main/java/net/junhabaek/tddpractice/book/adapter/in/web/BookRequestDto.java
+++ src/main/java/net/junhabaek/tddpractice/book/adapter/in/web/BookRequestDto.java
@@ -1,14 +1,13 @@
 package net.junhabaek.tddpractice.book.adapter.in.web;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.ToString;
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.*;
 import net.junhabaek.tddpractice.book.domain.Money;
 import net.junhabaek.tddpractice.book.domain.Quantity;
 
 public interface BookRequestDto {
     @Getter
-    @AllArgsConstructor
     @ToString
     public static class RegisterBookRequest {
         private final String bookName;
@@ -16,5 +15,16 @@ public interface BookRequestDto {
         private final Money price;
         private final Long page;
         private final Quantity quantity;
+
+        @JsonCreator
+        public RegisterBookRequest(@JsonProperty("bookName")String bookName, @JsonProperty("authorName")String authorName,
+                                   @JsonProperty("price")Money price, @JsonProperty("page")Long page,
+                                   @JsonProperty("quantity")Quantity quantity) {
+            this.bookName = bookName;
+            this.authorName = authorName;
+            this.price = price;
+            this.page = page;
+            this.quantity = quantity;
+        }
     }
 }
---
+++ src/test/java/net/junhabaek/tddpractice/book/adapter/in/web/BookRequestDtoTest.java
@@ -0,0 +1,55 @@
+package net.junhabaek.tddpractice.book.adapter.in.web;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ObjectNode;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import java.math.BigInteger;
+
+public class BookRequestDtoTest {
+    public static ObjectMapper objectMapper;
+
+    @BeforeAll
+    public static void init(){
+        objectMapper = new ObjectMapper();
+    }
+
+    @Test
+    void givenObjectNodeWithBookRequestDtoFormat_whenConvertToBookRequestDto_thenAllFieldsShouldBeValid() throws JsonProcessingException {
+        //given
+        final String bookName = "tragedy of Y";
+        final String authorName = "Ellery Queen";
+        final long price = 18000L;
+        final long page = 200L;
+        final long quantity = 50L;
+
+        ObjectNode requestJsonNode = buildRequestJson(bookName, authorName, price, page, quantity);
+
+        //when
+        BookRequestDto.RegisterBookRequest registerBookRequest =
+                objectMapper.treeToValue(requestJsonNode, BookRequestDto.RegisterBookRequest.class);
+
+        //then
+        Assertions.assertEquals(bookName, registerBookRequest.getBookName());
+        Assertions.assertEquals(authorName, registerBookRequest.getAuthorName());
+        Assertions.assertEquals(BigInteger.valueOf(price), registerBookRequest.getPrice().getAmount());
+        Assertions.assertEquals(page, registerBookRequest.getPage());
+        Assertions.assertEquals(BigInteger.valueOf(quantity), registerBookRequest.getQuantity().getQuantity());
+    }
+
+    private ObjectNode buildRequestJson(String bookName, String authorName, Long price, Long page, Long quantity){
+        ObjectNode objectNode = objectMapper.createObjectNode();
+
+        objectNode.put("bookName", bookName);
+        objectNode.put("authorName", authorName);
+        objectNode.put("price", price);
+        objectNode.put("page", page);
+        objectNode.put("quantity", quantity);
+
+        return objectNode;
+    }
+
+}
